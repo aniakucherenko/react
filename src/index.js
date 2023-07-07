a68db2ad420a53e4eb18a4a8f76cfefd8a17a085
aniakucherenko <ania.kucherenko@gmail.com>
2023-07-07 12:49:31 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -2,9 +2,24 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { App } from 'App';
 import './index.css';
+import {createGlobalStyle} from 'styled-components'
+
+
+const Global = createGlobalStyle`
+body{
+  margin: 0;
+  padding: 0;
+}
+p{
+  padding-left:20px
+}
+
+`
+
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
+    <Global />
   </React.StrictMode>
 );
