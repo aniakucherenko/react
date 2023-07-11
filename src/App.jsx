09ac640ede727eac91eff0314d977c35a33804d7
aniakucherenko <ania.kucherenko@gmail.com>
2023-07-11 12:52:39 +0200

    update

--- src/App.jsx
+++ src/App.jsx
@@ -2,10 +2,12 @@ import React from 'react'
 import { Modal } from "./components/Modal/Modal";
 import { Users } from "./components/Users/Users";
 import usersData from "./assets/users.json"
+import { Counter } from 'components/Counter/Counter';
 
 export const App = () => {
   return (
     <>
+      <Counter />
       <Modal>
         <h1>Hello</h1>
         <button>OK</button>
--- src/components/Counter/Counter.jsx
+++ src/components/Counter/Counter.jsx
@@ -1,7 +1,56 @@
-import React from 'react'
+import React, {Component} from 'react'
+import { Flex, FlexContainer, StyledButton, StyledCounter } from './Counter.styled'
 
-export const Counter = () => {
-  return (
-    <div>Counter</div>
-  )
+
+
+export class Counter extends Component {
+    state = {
+      counter: 10
+  }
+increment = () => {
+  this.setState({counter: this.state.counter +1})
 }
+decrement = () => {
+  this.setState({counter: this.state.counter -1})
+}
+
+reset = () => {
+  this.setState({counter:0})
+}
+ 
+  render() {
+    return (
+      <FlexContainer>
+        <StyledCounter>
+          <h1>{this.state.counter}</h1>
+          <Flex>
+            <StyledButton onClick={this.decrement}>minus</StyledButton>
+            <StyledButton onClick={this.reset}>reset</StyledButton>
+            <StyledButton onClick={this.increment}>plus</StyledButton>
+          </Flex>
+        </StyledCounter>
+      </FlexContainer>  
+        )
+  }
+}
+
+
+
+// export const Counter = () => {
+// const handleClick = e => {
+//   console.log(e)
+// }
+
+//   return (
+// <FlexContainer>
+//   <StyledCounter>
+//     <h1>{0}</h1>
+//     <Flex>
+//       <StyledButton onClick={handleClick} >minus</StyledButton>
+//       <StyledButton>reset</StyledButton>
+//       <StyledButton>plus</StyledButton>
+//     </Flex>
+//   </StyledCounter>
+// </FlexContainer>  
+//   )
+// }
