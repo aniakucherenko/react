82dcea3d01a7f196cfe947ccd6af497aeebb1df6
aniakucherenko <ania.kucherenko@gmail.com>
2023-07-19 13:15:34 +0200

    Update ToDoList.jsx

--- src/components/ToDoList/ToDoList.jsx
+++ src/components/ToDoList/ToDoList.jsx
@@ -6,7 +6,15 @@ import todoData from './../../assets/todos.json'
 
 export class ToDoList extends Component {
 state = {
- tasks: todoData
+ tasks: todoData,
+ inputValue: '',
+}
+
+handleToggleToDo = id => {
+console.log(id)
+this.setState(prevState => ({
+    tasks: prevState.tasks.map(task => task.id === id ? {...task, completed: !task.completed} : task)
+}))
 }
 
 handleDelete = (id) => {
@@ -15,21 +23,37 @@ handleDelete = (id) => {
  this.setState({tasks:newArray})
 }
 
+handleAddToDo = () => {
+    console.log('add')
+    this.setState({tasks: [
+        ...this.state.tasks,
+        {
+            id:crypto.randomUUID(),
+            todo: this.state.inputValue,
+            completed:false,
+        }
+    ]})
+}
+
+handleResetToDo = () => {
+this.setState({tasks: []})
+}
+
 
     render(){
-        const {tasks} = this.state
+        const {tasks, inputValue} = this.state
         return (
 <div>
       <StyledTodoList>
-        <StyledInput type ='text'/>
-        <StyledButton>Add
+        <StyledInput type ='text' value={inputValue} onChange = { e=> this.setState({inputValue: e.target.value})}/>
+        <StyledButton onClick = {this.handleAddToDo}>Add
         </StyledButton>
         {tasks.map(item => (<StyledTodo key= {item.id}>
-            <input type='checkbox'/>
+            <input type='checkbox' checked={item.completed} onChange={() =>this.handleToggleToDo(item.id)}/>
             <span>{item.todo}</span>
             <StyledButton size = '18px' onClick={()=> this.handleDelete(item.id)}>Delete</StyledButton>
         </StyledTodo>))}
-        <button>Clear</button>
+        <button onClick={this.handleResetToDo}>Clear</button>
       </StyledTodoList>
     </div>
         )
